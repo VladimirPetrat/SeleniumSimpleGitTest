e4ad7710c91bb41f3c4683d91074e9993c45b362
Vladimir Petrat <VladimirPetrat@gmail.com>
2021-03-08 10:31:53 +0200

    Updated gitignore

--- src/test/java/org/example/ConfProperties.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.FileInputStream;
-import java.util.Properties;
-
-public class ConfProperties {
-    private static FileInputStream fileInputStream;
-    private static Properties props;
-}
